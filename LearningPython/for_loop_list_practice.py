3ee16508326c3df256d3a50480f93b92d223b651
sri77ram <sri77ram@gmail.com>
2024-11-09 20:15:11 -0600

    Add files via upload

---
+++ LearningPython/for_loop_list_practice.py
@@ -0,0 +1,11 @@
+import random
+import questions_by_avix
+
+main_list = questions_by_avix.for_loop_question_random()
+
+# The list above contains a 2D matrix. It would be something like this: [[10, 20], [40, 30], [2, 269]].
+# It will be with a lot more numbers. The question is to print ALL of the numbers inside the list.
+print(main_list)#[0])
+# for j in range(0,len(main_list)):
+#     for i in range(0, len(main_list[j])):
+#         print(main_list[j][i])
---
+++ LearningPython/questions_by_avix.py
@@ -0,0 +1,14 @@
+import random
+
+
+def for_loop_question_random():
+    main_list = []
+    temp_list = []
+    for i in range(25):
+        temp_list = []
+        for x in range(25):
+            temp_list.append(random.randint(0, 255))
+        main_list.append(temp_list)
+        temp_list = 0
+
+    return main_list
